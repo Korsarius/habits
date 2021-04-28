62c60138bfd84f13e12b99fa25614f4da252d9d8
AngelaDmytreno <angeladmytrenko22@gmail.com>
2021-04-28 15:12:14 +0300

    WIP: create view login page and login form

--- src/app/login-page/login-page.module.ts
+++ src/app/login-page/login-page.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { LoginPageComponent } from './login-page.component';
 
 @NgModule({
   declarations: [LoginPageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule,
+            RouterModule,
+            ReactiveFormsModule,
+            FormsModule,
+            MatFormFieldModule,
+            MatInputModule],
 })
 export class LoginPageModule {}
