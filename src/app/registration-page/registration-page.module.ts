b6c69f83be95b34ee2be193cf99abe3a949cb520
Alexey Lomov <45512319+Korsarius@users.noreply.github.com>
2021-04-28 15:42:34 +0300

    Revert "1"

--- src/app/registration-page/registration-page.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RegistrationPageComponent } from './registration-page.component';
-
-@NgModule({
-  declarations: [RegistrationPageComponent],
-  imports: [CommonModule],
-})
-export class RegistrationPageModule {}
