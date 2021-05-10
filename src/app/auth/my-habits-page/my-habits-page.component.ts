efe4d08bfbe00f7fcaad68cbf0f7c9b6d4884e4d
AngelaDmytreno <angelagolovko22@gmail.com>
2021-05-11 00:24:18 +0300

    WIP: create table with pagination

--- src/app/auth/my-habits-page/my-habits-page.component.ts
+++ src/app/auth/my-habits-page/my-habits-page.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { MatDialog } from '@angular/material/dialog';
+
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
 import { HabitDialogComponent } from 'src/app/shared/components/habit-dialog/habit-dialog.component';
+import { IHabit } from 'src/app/shared/interfaces';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-my-habits-page',
@@ -9,9 +16,30 @@ import { HabitDialogComponent } from 'src/app/shared/components/habit-dialog/hab
   styleUrls: ['./my-habits-page.component.scss'],
 })
 export class MyHabitsPageComponent implements OnInit {
-  constructor(public dialog: MatDialog) {}
 
-  ngOnInit(): void {}
+  allHabitsList: Array<IHabit>;
+
+
+  displayedColumns: string[] = ['Index', 'Habit Name', 'Frequency', 'Type', 'Public', 'Delete'];
+  dataSource: MatTableDataSource<IHabit>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(public dialog: MatDialog, private auth: AuthService) { }
+
+  ngOnInit(): void {
+    this.auth.getHabits().subscribe(res => {
+      this.allHabitsList = res;
+      console.log('all', this.allHabitsList);
+      this.dataSource = new MatTableDataSource(this.allHabitsList);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+    },
+      err => console.log(err)
+    );
+  }
+
 
   openDialog() {
     const dialogRef = this.dialog.open(HabitDialogComponent);
--- src/app/auth/my-habits-page/my-habits-page.module.ts
+++ src/app/auth/my-habits-page/my-habits-page.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table'; 
+import { MatPaginatorModule } from '@angular/material/paginator';
+
 
 import { MyHabitsPageComponent } from './my-habits-page.component';
 
 @NgModule({
   declarations: [MyHabitsPageComponent],
-  imports: [CommonModule, MatDialogModule],
+  imports: [CommonModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatTableModule, MatPaginatorModule],
   exports: [MyHabitsPageComponent],
 })
 export class MyHabitsPageModule {}
