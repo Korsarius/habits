770cd866e6ed082289de38fafa197e6cf53dc628
alroy <alroy@gmail.com>
2021-05-10 19:03:07 +0300

    WIP: 17 task

--- src/app/habits-page/habits-page.component.ts
+++ src/app/habits-page/habits-page.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IHabit } from '../shared/interfaces';
 
 @Component({
   selector: 'app-habits-page',
   templateUrl: './habits-page.component.html',
-  styleUrls: ['./habits-page.component.scss']
+  styleUrls: ['./habits-page.component.scss'],
 })
 export class HabitsPageComponent implements OnInit {
+  @Input() habit: IHabit;
+  habits: IHabit[] = new Array<IHabit>();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shared/components/habit-card/habit-card.component.ts
+++ src/app/shared/components/habit-card/habit-card.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { IHabit } from '../../interfaces';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-habit-card',
   templateUrl: './habit-card.component.html',
-  styleUrls: ['./habit-card.component.scss']
+  styleUrls: ['./habit-card.component.scss'],
 })
 export class HabitCardComponent implements OnInit {
+  @Input() habit: IHabit;
+  habits: Array<IHabit> = new Array<IHabit>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private auth: AuthService, private db: AngularFireDatabase) {
+    const habits = db.list('habits');
   }
 
+  ngOnInit(): void {}
+
+  addToMyHabits(): void {}
 }
