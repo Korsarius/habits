d83b4db0f9fe2b36b31803d4ea29c02193e232ed
AngelaDmytreno <angelagolovko22@gmail.com>
2021-05-15 22:25:19 +0300

    26 was done

--- src/app/habits-page/habits-page.component.ts
+++ src/app/habits-page/habits-page.component.ts
@@ -9,13 +9,43 @@ import { AuthService } from '../shared/services/auth.service';
 })
 export class HabitsPageComponent implements OnInit {
   habits: IHabit[] = new Array<IHabit>();
+  startCard: number = 0;
+  endCard: number = 6;
+  selectedHabits: IHabit[];
+  loadCount: number = 6;
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
     this.auth.getHabits().subscribe((res) => {
       this.habits = res;
+      this.selectedHabits = this.habits.slice(0, this.loadCount);
+
       console.log(this.habits);
     });
+  }
+
+
+
+  loadMore(): void {
+
+    this.startCard = this.endCard;
+    this.endCard += 6;
+    this.selectedHabits.push(...this.habits.slice(this.startCard, this.endCard));
+  }
+  rollUp(): void {
+    this.selectedHabits = this.habits.slice(0, this.loadCount);
+    this.resetStartEnd();
+  }
+
+  isShowRollUpButton(): boolean | void {
+    if (this.selectedHabits) {
+      return this.selectedHabits.length >= 6 && this.selectedHabits.length !== this.habits.length;
+    }
+  }
+
+  resetStartEnd(): void {
+    this.startCard = 0;
+    this.endCard = 6;
   }
 }
--- src/app/habits-page/habits-page.module.ts
+++ src/app/habits-page/habits-page.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HabitsPageComponent } from './habits-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HabitsPageComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, MatButtonModule],
 })
 export class HabitsPageModule {}
