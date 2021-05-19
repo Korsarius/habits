d09031335d27cd0ef47483166b32dad3a14050cc
alroy <alroy@gmail.com>
2021-05-19 20:32:05 +0300

    52 was done. Progress spinner is working

--- src/app/auth/my-habits-page/my-habits-page.module.ts
+++ src/app/auth/my-habits-page/my-habits-page.module.ts
@@ -1,24 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input'; 
-import { MatButtonModule } from '@angular/material/button'; 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table'; 
+import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; 
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { MyHabitsPageComponent } from './my-habits-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule } from '@angular/forms';
 
-
 @NgModule({
   declarations: [MyHabitsPageComponent],
-  imports: [CommonModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatTableModule, MatPaginatorModule, SharedModule, FormsModule, MatProgressSpinnerModule],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    MatPaginatorModule,
+    SharedModule,
+    FormsModule,
+    MatProgressSpinnerModule,
+    BrowserAnimationsModule,
+  ],
   exports: [MyHabitsPageComponent],
 })
 export class MyHabitsPageModule {}
